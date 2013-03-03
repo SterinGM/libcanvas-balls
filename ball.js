bd2180f99325089980b11aaf18bf6d468db05637
Sterin Grigoriy <muxajibi41981@gmail.com>
2013-03-03 14:56:10 +0300

    первые наброски для игры

---
+++ ball.js
@@ -0,0 +1,86 @@
+atom.declare('Balls.Ball', App.Element, {
+	configure: function () {
+		this.animate = new atom.Animatable(this).animate;
+
+        this.stable  = false;
+
+//		new App.Clickable( this, this.redraw ).start();
+	},
+
+    get controller () {
+		return this.settings.get('controller');
+	},
+
+	get position () {
+		return this.settings.get('position');
+	},
+
+    get from () {
+		return this.settings.get('from');
+	},
+
+	fall: function (point) {
+		var props = {}, current = this.shape.from;
+
+        var next = new Point(this.position.x, point ? this.position.y : this.from.y + 1);
+
+        var destination = this.controller.translatePoint(next);
+
+		if (!destination.x.equals(current.x, 1)) {
+			props.x = destination.x;
+		}
+
+        if (!destination.y.equals(current.y, 1)) {
+			props.y = destination.y;
+		}
+
+		this.animate({
+			time : point ? 1000 : 50,
+			fn   : point ? 'cubic-in' : 'linear',
+			props: props,
+			onTick: this.redraw,
+			onComplete: function () {
+				this.redraw();
+
+                this.stable = true;
+
+                if (next.y !== this.position.y) {
+                    this.fall(next);
+
+                    if (this.position.y > 0)
+                    {
+                        var ball = this.controller.balls[this.position.y - 1][this.position.x];
+
+                        if (ball && !ball.stable) {
+                            ball.fall();
+                        }
+                    }
+                } else {
+//                    this.controller.sounds.play('fall');
+                }
+			}.bind(this)
+		});
+
+		return this;
+	},
+
+	get x () { return this.shape.from.x },
+	get y () { return this.shape.from.y },
+
+	set x (value) {
+		return this.shape.move(new Point( value - this.x, 0 ));
+	},
+	set y (value) {
+		return this.shape.move(new Point( 0, value - this.y ));
+	},
+
+	renderTo: function (ctx) {
+        var image = this.settings.values.image;
+
+		ctx.drawImage({
+			image:    image,
+			draw :    this.shape,
+			optimize: true
+		});
+	}
+});
---
+++ controller.js
@@ -0,0 +1,141 @@
+atom.declare('Balls.Controller', {
+	initialize: function(settings) {
+		this.settings = new atom.Settings(settings);
+
+        atom.ImagePreloader.run({
+            white:   'balls.png [15:15:120:120]',
+            red:     'balls.png [15:165:120:120]',
+            yellow:  'balls.png [15:315:120:120]',
+            orange:  'balls.png [15:473:120:120]',
+            blue:    'balls.png [15:615:120:120]',
+            magenta: 'balls.png [15:765:120:120]',
+            green:   'balls.png [15:915:120:120]'
+        }, this.start.bind(this));
+	},
+
+    start: function (images) {
+		var app, layer, mouse;
+
+        this.images = images;
+
+//        this.sounds = new Balls.Sounds('/mp3/');
+
+		app = new App({
+			size: this.size()
+		});
+
+        this.createField(app);
+
+        this.layer = app.createLayer({intersection: 'manual', name: 'balls', zIndex: 1});
+
+        this.generate();
+    },
+
+    createField: function(app) {
+        var layerFiels = app.createLayer({name: 'field', zIndex: 0});
+
+        var size = this.settings.get('size');
+
+		var
+            y, x, position, odd;
+
+		for (y = 0; y < size.y; y++) {
+            odd = y % 2 ;
+
+			for (x = 0; x < size.x; x++) {
+				position = new Point(x, y);
+
+                odd = !odd;
+
+				new Balls.Field(layerFiels, {
+                    position: position,
+                    shape:    this.tileShape(position),
+                    odd:      odd
+                });
+			}
+		}
+    },
+
+    size: function() {
+		var tile = this.settings.get('tile');
+        var size = this.settings.get('size');
+
+		return new Size(
+			size.x * (tile.width  + tile.margin) + tile.margin,
+			size.y * (tile.height + tile.margin) + tile.margin
+		);
+	},
+
+    generate: function() {
+        var size = this.settings.get('size');
+
+		var
+			i, y, x, position,
+			balls = {};
+
+        this.balls = atom.array.fillMatrix(size.x, size.y, 0);
+
+		for (y = size.y - 1; y >= 0; y--) {
+			for (x = 0; x < size.x; x++) {
+                var position = new Point(x, y);
+
+                this.balls[y][x] = this.createTile(this.layer, position);
+			}
+		}
+
+        this.fallBalls();
+
+		return this;
+	},
+
+    fallBalls: function() {
+        var size = this.settings.get('size');
+
+        for (x = 0; x < size.x; x++) {
+            for (y = size.y - 1; y >= 0; y--) {
+                if (this.balls[y][x] && !this.balls[y][x].stable) {
+                    this.balls[y][x].fall();
+
+                    break;
+                }
+            }
+        }
+    },
+
+    createTile: function(layer, position) {
+        var size = this.settings.get('size');
+        var colors = ['white', 'red', 'green', 'blue', 'yellow', 'orange', 'magenta'];
+
+        var pos = new Point(position.x, position.y - size.y - 1);
+
+		var tile = new Balls.Ball(layer, {
+            from:       pos,
+			position:   position,
+			shape:      this.tileShape(pos),
+            image:      this.images.get(colors.popRandom()),
+            controller: this
+		});
+
+//		this.mouseHandler.subscribe(tile);
+//
+//		tile.events.add( 'mousedown', function (e) {
+//			e.preventDefault();
+//			this.move( tile );
+//		}.bind(this));
+
+		return tile;
+	},
+
+	tileShape: function(position) {
+        return new Rectangle({
+			from: this.translatePoint(position),
+			size: this.settings.get('tile')
+		});
+	},
+
+	translatePoint: function(position) {
+		var tile = this.settings.get('tile');
+
+		return new Point(position.x * (tile.width + tile.margin) + tile.margin, position.y * (tile.height + tile.margin) + tile.margin);
+	}
+});
